48f4f0799b4fe491678d0d90c6394d96e2878df6
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-24 22:56:30 +0000

    form validation and game over state

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
-var players = [100, 100];
+function updatePlayerStatus(money){
+  $('#player_status').html('You have $'+money);
+}
+
+function checkBetAmount(bet){
+  return bet < 5 || bet > 10;
+}
+
+function checkNegativeAmount(money, bet){
+  var betCheck = money - bet;
+  
+  return betCheck < 0;
+}
+
+function updateGameMessage(msg){
+  $('#game_message').html(msg);
+}
 
 $( document ).ready(function() {
+    
+    //player 1 has $100
+    var playerMoney = 100;
+    updatePlayerStatus(playerMoney);
+
     $( "#game_form" ).submit(function( event ) {
-      
       event.preventDefault();
+
+      //lock down game
+      if(playerMoney < 5){
+        return
+      }
+      
+      var guess = +$('#guess').val();
+      var bet = +$('#bet').val();
+      var error_msg = "";
+
+      if(!$.isNumeric(guess)){
+        error_msg += 'Your guess isn\'t a number. ';
+      }
+
+      if(checkBetAmount(bet)) {
+        error_msg += 'Bets must be between 5 to 10 dollars. ';
+      }
+
+      if(checkNegativeAmount(playerMoney, bet)){
+        error_msg += 'You don\'t have enough money to bet that amount. ';
+      }
+
+      //no errors means normal play
+      if(error_msg === ""){
+
+        //number to be guessed
+        randomNum = Math.floor((Math.random() * 10) + 1);
+        console.log(guess, bet);
+        playerMoney /= 2;
+        updatePlayerStatus(playerMoney);
+
+        //let the player know it's over
+        if(playerMoney < 5){
+          updateGameMessage("Game Over")
+        }
+
+      }else{
+
+        updateGameMessage(error_msg);
+        
+      }
+
     });
 });
